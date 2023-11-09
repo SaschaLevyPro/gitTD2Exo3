cac7404db57f1e156b7ee99e040b3af5461bd5da
Thomas Loye <thomas.loye@etu.umontpellier.fr>
2023-11-09 11:47:05 +0100

    fix : crash git

--- src/main/java/fr/iut/editeur/document/Document.java
+++ src/main/java/fr/iut/editeur/document/Document.java
@@ -2,13 +2,13 @@ package fr.iut.editeur.document;
 
 public class Document {
 
-<<<<<<< src/main/java/fr/iut/editeur/document/Document.java
     private String texteDocument;
+    private String contentDocument;
 
     public Document() {
         this.texteDocument = "";
     }
-	
+
     public String getTexte() {
         return texteDocument;
     }
@@ -19,13 +19,8 @@ public class Document {
 
     public void ajouter(String texte) {
         this.texteDocument += texte;
-=======
-    private String contentDocument;
-
-    public Document() {
-        this.contentDocument = "";
     }
-	
+
     public String getContentDocument() {
         return contentDocument;
     }
@@ -34,28 +29,10 @@ public class Document {
         this.contentDocument = contentDocument;
     }
 
-    public void ajouter(String texte) {
-        this.contentDocument += texte;
->>>>>>> src/main/java/fr/iut/editeur/document/Document.java
-    }
 
     @Override
     public String toString() {
-<<<<<<< src/main/java/fr/iut/editeur/document/Document.java
         return this.texteDocument;
-    }
-
-    public void remplacer(int start, int end, String remplacement) {
-        String leftPart = texteDocument.substring(0, start);
-        String rightPart = texteDocument.substring(end);
-        texteDocument = leftPart + remplacement + rightPart;
-    }
-    public void majuscules(int start, int end) {
-        remplacer(start, end, texteDocument.substring(start, end).toUpperCase());
-    }
-    public void minuscules(int start, int end) {remplacer(start, end, texteDocument.substring(start, end).toLowerCase());}
-=======
-        return this.contentDocument;
     }
 
     public void remplacer(int start, int end, String remplacement) {
@@ -63,30 +40,28 @@ public class Document {
         String rightPart = contentDocument.substring(end);
         contentDocument = leftPart + remplacement + rightPart;
     }
+
     public void majuscules(int start, int end) {
         remplacer(start, end, contentDocument.substring(start, end).toUpperCase());
     }
-    public void minuscules(int start, int end) {remplacer(start, end, contentDocument.substring(start, end).toLowerCase());}
->>>>>>> src/main/java/fr/iut/editeur/document/Document.java
+
+    public void minuscules(int start, int end) {
+        remplacer(start, end, contentDocument.substring(start, end).toLowerCase());
+    }
+
     public void effacer(int start, int end) {
         remplacer(start, end, "");
     }
+
     public void clear() {
-<<<<<<< src/main/java/fr/iut/editeur/document/Document.java
         texteDocument = "";
     }
+
     public void inserer(int position, String texte) {
         String leftPart = this.texteDocument.substring(0, position);
         String rightPart = this.texteDocument.substring(position);
         this.texteDocument = leftPart + texte + rightPart;
-=======
         contentDocument = "";
-    }
-    public void inserer(int position, String texte) {
-        String leftPart = this.contentDocument.substring(0, position);
-        String rightPart = this.contentDocument.substring(position);
-        this.contentDocument = leftPart + texte + rightPart;
->>>>>>> src/main/java/fr/iut/editeur/document/Document.java
     }
 
 }
