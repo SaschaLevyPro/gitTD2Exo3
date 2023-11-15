3ea89789d0940886077db7dd08284534998e2c41
YvelGY <lpttgrand@gmail.com>
2023-11-15 10:17:05 +0100

    feat : ajout réel des tests

--- src/test/java/fr/iut/editeur/document/DocumentTest.java
+++ src/test/java/fr/iut/editeur/document/DocumentTest.java
@@ -1,42 +1,50 @@
 package fr.iut.editeur.document;
 
+import org.junit.Test;
+
 public class DocumentTest extends junit.framework.TestCase{
+    @Test
     public void testAjouter() throws Exception {
         Document document = new Document();
         document.ajouter("Bonjour");
         assertEquals("Bonjour", document.toString());
     }
+    @Test
     public void testClear() throws Exception {
         Document document = new Document();
         document.ajouter("Bonjour");
         document.clear();
         assertEquals("", document.toString());
     }
+    @Test
     public void testRemplacer() throws Exception {
         Document document = new Document();
         document.ajouter("Bonjour");
         document.remplacer(0, 3, "Bonsoir");
         assertEquals("Bonsoirjour", document.toString());
     }
+    @Test
     public void testMajuscules() throws Exception {
         Document document = new Document();
         document.ajouter("Bonjour");
         document.majuscules(0, 3);
         assertEquals("BONjour", document.toString());
     }
+    @Test
     public void testEffacer() throws Exception {
         Document document = new Document();
         document.ajouter("Bonjour");
         document.effacer(0, 3);
         assertEquals("jour", document.toString());
     }
+    @Test
     public void testInserer() throws Exception {
         Document document = new Document();
         document.ajouter("Bonjour");
         document.inserer(3, "soir");
         assertEquals("Bonsoirjour", document.toString());
     }
-
+    @Test
     public void testMinuscules() throws Exception {
         Document document = new Document();
         document.ajouter("BONJOUR");
